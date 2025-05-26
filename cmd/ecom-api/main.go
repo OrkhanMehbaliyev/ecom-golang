f20487a77114aee5bddfaf3a9c1f76983abd4096
Orkhan <mehbaliyevorxan2003@gmail.com>
2025-05-26 21:12:35 +0400

    authentication and users endpoint

--- cmd/ecom-api/main.go
+++ cmd/ecom-api/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/OrkhanMehbaliyev/ecom-golang/db"
 	"github.com/OrkhanMehbaliyev/ecom-golang/ecom-api/handler"
 	"github.com/OrkhanMehbaliyev/ecom-golang/ecom-api/server"
 	"github.com/OrkhanMehbaliyev/ecom-golang/ecom-api/storer"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("error loading .env file: %s", err)
+	}
+
 	db, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("error opening database: %v", err)
@@ -19,8 +26,9 @@ func main() {
 
 	st := storer.NewMySQLStorer(db.GetDB())
 	srv := server.NewServer(st)
-	hdl := handler.NewHandler(srv)
+	hdl := handler.NewHandler(srv, os.Getenv("SECRET_KEY"))
 	handler.RegisterRouters(hdl)
+
 	err = handler.Start(":8080")
 	if err != nil {
 		log.Fatal("error starting server: %w", err)
